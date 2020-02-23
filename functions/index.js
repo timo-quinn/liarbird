3cbeebafa6ece75254f82fa479fdcc2f9b486a99
Tim <tim@congruentlabs.co>
2020-02-23 18:57:26 +1100

    some notes for the first function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.registerDevice = functions.https.onRequest((request, response) => {
+  // check the payload for sufficient info
+  // generate a unique device id for it
+  // add the record to the database
+  // return the device id to the device for it to store in itself
+});
